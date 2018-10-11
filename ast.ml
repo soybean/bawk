2ff7381a0773c35c7f0916caed6dab72964594dc
Ashley An <ashley.an@columbia.edu>
2018-10-10 20:30:11 -0400

    Update ast.ml

--- ast.ml
+++ ast.ml
@@ -1,6 +1,6 @@
 type op =  Add | Strcat | And | Or | Sub | 
-Mult | Div | Inc | Dec | Pluseq | Mineq |
-Leq | Geq | Less | Greater | Assn | Equal | Neq |
-Rgx | Rgxcomp | Rgxneq | Rgxeq
+           Mult | Div | Inc | Dec | Pluseq | Mineq |
+           Leq | Geq | Less | Greater | Assn | Equal | Neq |
+           Rgx | Rgxcomp | Rgxneq | Rgxeq
 type uop = Neg | Not | Dollar | Index
 type typ = Int | Bool | String | Rgx | Arr | Map
