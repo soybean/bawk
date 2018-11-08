ea3518fe2e9f185791a925f5aa40efdfb222bf10
Victoria Yang <victoria.j.yang@gmail.com>
2018-11-07 19:42:33 -0500

    Changed sast.ml to ast.ml

--- sast.ml
+++ sast.ml
@@ -1,65 +1,54 @@
-type op = Add | Sub | Mult | Div | Assn | Equal | Neq | Less | Leq | Greater | 
-		  Geq | And | Or | Pluseq | Minuseq | Strcat | Rgxeq | Rgxneq | 
-		  Rgxcomp | Rgxnot
-type uop = Not | Access | Neg | AssignElement | GetElement | Increment | Decrement
-type typ = Int | Bool | Void | String | Rgx
-type bind = typ * string 
-type expr = Binop of expr * op * expr
-| BoolLit of bool
-| Literal of int
-| Id of string
-| Assign of string * expr
-| Call of string * expr list
-| Rgx of string
-| Unop of uop * expr
-| InitIntArrLit of string * expr list
-| InitStrArrLit of string * expr list
-| InitBoolArrLit of string * expr list
-| InitRgxArrLit of string * expr list
-| InitMapLit of typ * typ * string * expr list
-| InitEmptyMap of typ * typ * string
-| AssignElement of string * expr * expr
-| GetElement of string * expr
-| NumFields
+open Ast
 
-type config_expr = RSAssign of expr
-| FSAssign of expr
+type sbind = typ * string 
+type sexpr = typ * sx and
+sx = SBinop of sexpr * op * sexpr
+| SBoolLit of bool
+| SLiteral of int
+| SId of string
+| SAssign of string * sexpr
+| SCall of string * sexpr list
+| SRgx of string
+| SUnop of uop * sexpr
+| SInitIntArrLit of string * sexpr list
+| SInitStrArrLit of string * sexpr list
+| SInitBoolArrLit of string * sexpr list
+| SInitRgxArrLit of string * sexpr list
+| SInitMapLit of typ * typ * string * sexpr list
+| SInitEmptyMap of typ * typ * string
+| SAssignElement of string * sexpr * sexpr
+| SGetElement of string * sexpr
+| SNumFields
 
+type sconfig_expr = SRSAssign of sexpr
+| SFSAssign of sexpr
 
-type stmt = Return of expr
-| Expr of expr
-| Block of stmt list
-| While of expr * stmt
-| If of expr * stmt * stmt
-| InitEmptyMap of typ * typ * string
-| For of expr * expr * expr * stmt
-| EnhancedFor of typ * string * stmt
-| AssignElement of string * expr * expr
-| GetElement of string * expr
+type sstmt = SReturn of sexpr
+| SExpr of sexpr
+| SBlock of Sstmt list
+| SWhile of sexpr * sstmt
+| SIf of expr * sstmt * sstmt
+| SInitEmptyMap of typ * typ * string
+| SFor of sexpr * sexpr * sexpr * sstmt
+| SEnhancedFor of typ * string * sstmt
+| SAssignElement of string * sexpr * sexpr
+| SGetElement of string * sexpr
 
-type func_decl = {
-  ret_type : typ;
-  fname    : string;
-  formals  : bind list;
-  locals   : bind list;
-  body     : stmt list;
+type sfunc_decl = {
+  sret_type : typ;
+  sfname    : string;
+  sformals  : sbind list;
+  slocals   : sbind list;
+  sbody     : sstmt list;
 }
 
 
-type begin_list = bind list * func_decl list
+type sbegin_list = sbind list * sfunc_decl list
 
-type loop_list = bind list * stmt list
+type sloop_list = sbind list * sstmt list
 
-type end_list = bind list * stmt list
+type send_list = sbind list * sstmt list
 
-type config_list = config_expr list
+type sconfig_list = sconfig_expr list
 
-
-type program = begin_list * loop_list * end_list * config_list
-
-
-(*type program = func_decl list * bind list * func_decl list * bind list * func_decl list * bind list * func_decl list * bind list*)
-(*type program = bind list * func_decl list*)
-
-
-let string_of_program(beginBlock, loop, endBlock, configBlock) = "HI"
+type sprogram = sbegin_list * sloop_list * send_list * sconfig_list
