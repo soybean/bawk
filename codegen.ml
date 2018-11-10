92529fa2f7850a48a1404a285239cb94aa90b624
Christine Hsu <christine.hhsu@gmail.com>
2018-11-10 16:15:52 -0500

    updated codegen, does not work still

--- codegen.ml
+++ codegen.ml
@@ -11,7 +11,7 @@ let translate (begin_block, loop_block, end_block, config_block) =
 	let the_module = L.create_module context "Bawk" in
 
 	(* Get types from the context *)
-	(* let i32_t      = L.i32_type    context
+	let i32_t      = L.i32_type    context
 	and i8_t       = L.i8_type     context
 	and i1_t       = L.i1_type     context
 	and float_t    = L.double_type context
@@ -21,22 +21,26 @@ let translate (begin_block, loop_block, end_block, config_block) =
 	let ltype_of_typ = function
 	  A.Int   -> i32_t
 	| A.Bool  -> i1_t
-	| A.Float -> float_t
 	| A.Void  -> void_t
-	in *)
+
+	in
 
 	let printf_t : L.lltype = 
       L.var_arg_function_type i32_t [| L.pointer_type i8_t |] in
   	let printf_func : L.llvalue = 
       L.declare_function "printf" printf_t the_module in
 
-    let build_end_block block =
+    let build_end_block =
+    	let builder = L.builder_at_end context (L.entry_block the_function) in
+    	
     	let string_format_str builder = L.build_global_stringptr "%s\n" "fmt" builder in
 
     	let rec expr builder = function
     		A.Call ("print", [e]) ->
     			L.build_call printf_func [| string_format_str builder; (expr builder e)|] "printf" builder
+
+    	in
 	
-    in build_end_block end_block;
+    build_end_block
 
     in the_module
