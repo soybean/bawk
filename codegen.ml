1f035b70cc5992c26987c8a611f627e6efaf953f
Victoria Yang <victoria.j.yang@gmail.com>
2018-11-10 18:31:08 -0500

    Remove float

--- codegen.ml
+++ codegen.ml
@@ -14,7 +14,6 @@ let translate (begin_block, loop_block, end_block, config_block) =
 	let i32_t      = L.i32_type    context
 	and i8_t       = L.i8_type     context
 	and i1_t       = L.i1_type     context
-	and float_t    = L.double_type context
 	and void_t     = L.void_type   context in
 
 	(* Return the LLVM type for a MicroC type *)
