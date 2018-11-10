0cede7240d81c80f2d70294f6e1b370f265425ea
Victoria Yang <victoria.j.yang@gmail.com>
2018-11-10 17:07:02 -0500

    Added enhanced for

--- semant.ml
+++ semant.ml
@@ -172,12 +172,17 @@ ArrayLit of expr list
       in if t' != Bool then raise (Failure err) else (t', e') 
     in
 
+(*TODO:
+| InitEmptyMap of typ * typ * string
+| EnhancedFor of string * stmt*)
     (* Return a semantically-checked statement i.e. containing sexprs *)
     let rec check_stmt = function
         Expr e -> SExpr (expr e)
       | If(p, b1, b2) -> SIf(check_bool_expr p, check_stmt b1, check_stmt b2)
       | For(e1, e2, e3, st) ->
 	  SFor(expr e1, check_bool_expr e2, expr e3, check_stmt st)
+      | EnhancedFor(s1, st) ->
+          SEnhancedFor(String s1, check_stmt st)
       | While(p, s) -> SWhile(check_bool_expr p, check_stmt s)
       | Return e -> let (t, e') = expr e in
         if t = func.typ then SReturn (t, e') 
