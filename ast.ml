b3ef0f10f8c00eedfcbb8ee814e71e5a794349a4
Ashley An <ashley.an@columbia.edu>
2018-11-14 21:30:26 -0500

    cleaned up code

--- ast.ml
+++ ast.ml
@@ -1,42 +1,49 @@
 type op = Add | Sub | Mult | Div | Assn | Equal | Neq | Less | Leq | Greater | 
-		  Geq | And | Or | Pluseq | Minuseq | Strcat | Rgxeq | Rgxneq | 
-		  Rgxcomp | Rgxnot
+          Geq | And | Or | Pluseq | Minuseq | Strcat | Rgxeq | Rgxneq | 
+          Rgxcomp | Rgxnot
+
 type uop = Not | Access | Neg | Increment | Decrement
+
 type typ = Int | Bool | Void | String | Rgx | ArrayType of typ
+
 type bind = typ * string 
-type expr = Binop of expr * op * expr
-| BoolLit of bool
-| Literal of int
-| StringLiteral of string
-| RgxLiteral of string
-| Id of string
-| Assign of expr * expr
-| Call of string * expr list
-| Rgx of string
-| Unop of uop * expr
-| ArrayLit of expr list
-| ArrayDeref of expr * expr
-| NumFields
-| Noexpr
 
-type config_expr = RSAssign of expr
-| FSAssign of expr
+type expr = 
+    Binop of expr * op * expr
+  | BoolLit of bool
+  | Literal of int
+  | StringLiteral of string
+  | RgxLiteral of string
+  | Id of string
+  | Assign of expr * expr
+  | Call of string * expr list
+  | Rgx of string
+  | Unop of uop * expr
+  | ArrayLit of expr list
+  | ArrayDeref of expr * expr
+  | NumFields
+  | Noexpr
 
-type stmt = Return of expr
-| Expr of expr
-| Block of stmt list
-| While of expr * stmt
-| If of expr * stmt * stmt
-| For of expr * expr * expr * stmt
-| EnhancedFor of string * stmt
+type config_expr = 
+    RSAssign of expr
+  | FSAssign of expr
+
+type stmt = 
+    Return of expr
+  | Expr of expr
+  | Block of stmt list
+  | While of expr * stmt
+  | If of expr * stmt * stmt
+  | For of expr * expr * expr * stmt
+  | EnhancedFor of string * stmt
 
 type func_decl = {
-  ret_type : typ;
-  fname    : string;
-  formals  : bind list;
-  locals   : bind list;
-  body     : stmt list;
-}
+    ret_type : typ;
+    fname    : string;
+    formals  : bind list;
+    locals   : bind list;
+    body     : stmt list;
+  }
 
 type begin_list = bind list * func_decl list
 type loop_list = bind list * stmt list
@@ -47,5 +54,24 @@ type program = begin_list * loop_list * end_list * config_list
 
 (* Pretty-printing functions *)
 
+(*let string_of_op = function
+    Add -> "+"
+  | Sub -> "-"
+  | Mult -> "*"
+  | Div -> "/"
+  | Add | Sub | Mult | Div | Assn | Equal | Neq | Less | Leq | Greater | 
+		  Geq | And | Or | Pluseq | Minuseq | Strcat | Rgxeq | Rgxneq | 
+		  Rgxcomp | Rgxnot
+*)
+
+
+
+
+
+
+
+
 let string_of_program(beginBlock, loop, endBlock, configBlock) = "PASS\n"
+
+
 
--- sast.ml
+++ sast.ml
@@ -1,39 +1,42 @@
 open Ast
 
 type sexpr = typ * sx
-and sx = SBinop of sexpr * op * sexpr
-| SBoolLit of bool
-| SLiteral of int
-| SStringLiteral of string
-| SRgxLiteral of string
-| SId of string
-| SAssign of sexpr * sexpr
-| SCall of string * sexpr list
-| SRgx of string
-| SUnop of uop * sexpr
-| ArrayLit of sexpr list
-| ArrayDeref of sexpr * sexpr
-| SNumFields
-| SNoexpr
+and sx = 
+    SBinop of sexpr * op * sexpr
+  | SBoolLit of bool
+  | SLiteral of int
+  | SStringLiteral of string
+  | SRgxLiteral of string
+  | SId of string
+  | SAssign of sexpr * sexpr
+  | SCall of string * sexpr list
+  | SRgx of string
+  | SUnop of uop * sexpr
+  | ArrayLit of sexpr list
+  | ArrayDeref of sexpr * sexpr
+  | SNumFields
+  | SNoexpr
 
-type sconfig_expr = SRSAssign of sexpr
-| SFSAssign of sexpr
+type sconfig_expr = 
+    SRSAssign of sexpr
+  | SFSAssign of sexpr
 
-type sstmt = SReturn of sexpr
-| SExpr of sexpr
-| SBlock of sstmt list
-| SWhile of sexpr * sstmt
-| SIf of sexpr * sstmt * sstmt
-| SFor of sexpr * sexpr * sexpr * sstmt
-| SEnhancedFor of string * sstmt
+type sstmt = 
+    SReturn of sexpr
+  | SExpr of sexpr
+  | SBlock of sstmt list
+  | SWhile of sexpr * sstmt
+  | SIf of sexpr * sstmt * sstmt
+  | SFor of sexpr * sexpr * sexpr * sstmt
+  | SEnhancedFor of string * sstmt
 
 type sfunc_decl = {
-  sret_type : typ;
-  sfname    : string;
-  sformals  : bind list;
-  slocals   : bind list;
-  sbody     : sstmt list;
-}
+    sret_type : typ;
+    sfname    : string;
+    sformals  : bind list;
+    slocals   : bind list;
+    sbody     : sstmt list;
+  }
 
 type sbegin_list = bind list * sfunc_decl list
 type sloop_list = bind list * sstmt list
