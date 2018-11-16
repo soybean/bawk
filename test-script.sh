32894a3b031fd0e6577f30807979756c4e3ab71e
Ashley An <ashley.an@columbia.edu>
2018-11-16 12:13:49 -0500

    edited test script to create .out and .err files

--- test-script.sh
+++ test-script.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
-for filename in tests/*; do
-   echo $filename
-   ./bawk.native -a $filename -f ./input.txt
+substr="fail-*"
+for filename in tests/*.bawk; do
+  v=${filename::-5}
+  if [[ ${filename:${#filename}-5} = ".bawk" ]];
+  then
+    echo $filename
+    if [[ ${filename:0:10} = "tests/fail" ]];
+      then
+        ./bawk.native -s $filename -f ./input.txt 2> $v.err
+      else
+        ./bawk.native -s $filename -f ./input.txt > $v.out
+    fi
+  fi
 done
