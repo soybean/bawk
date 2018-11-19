e779dcd009b60642c8087ebc3f960113ce8d6970
Melanie Sawyer <melaniensawyer@gmail.com>
2018-11-19 16:49:53 -0500

    add convert.c

---
+++ convert.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* This function converts a string to an int */
+int string_to_int(char a[])
+{
+  return 0;
+  //return atoi(a);
+}
+
+char * int_to_string(int a) {
+  return "hey";
+}
