7863a293d1da96a8f22469c90b8e2fdf89868dc4
Christine Hsu <christine.hhsu@gmail.com>
2018-11-19 18:07:40 -0500

    add wildcard pattern matching to codegen

--- codegen.ml
+++ codegen.ml
@@ -45,7 +45,7 @@ let translate (begin_block, loop_block, end_block, config_block) input_file =
     List.fold_left global_var StringMap.empty (fst begin_block) in
 
 
-  (*--- Build begin block: functions ---*)
+  (*--- Build begin block: function declarations ---*)
   let function_decls : (L.llvalue * A.func_decl) StringMap.t =
     let function_decl m fdecl =
       let formal_types =
@@ -58,6 +58,14 @@ let translate (begin_block, loop_block, end_block, config_block) input_file =
     
   in
 
+  let add_terminal builder instr =
+    match L.block_terminator (L.insertion_block builder) with
+	    Some _ -> ()
+      | None -> ignore (instr builder)
+  
+  in
+
+  (*--- Build begin block: function body ---*)
   let build_function_body fdecl =
     let (the_function, _) = StringMap.find fdecl.A.fname function_decls in
     let func_builder = L.builder_at_end context (L.entry_block the_function) in
@@ -94,16 +102,40 @@ let translate (begin_block, loop_block, end_block, config_block) input_file =
 			| A.BoolLit b  -> L.const_int i1_t (if b then 1 else 0)
     	| A.Call ("print", [e]) ->
     		L.build_call printf_func [| string_format_str builder; (expr builder e)|] "printf" builder
+      | A.Noexpr -> L.const_int i32_t 0
+      | A.Id i -> L.build_load (lookup i) i builder
+      (* TODO: handle strings and rgx by building binop_gen function (see Decaf)*)
+      | A.Binop(e1, op, e2) ->
+        let e1' = expr builder e1
+        and e2' = expr builder e2 in
+        (match op with
+          A.Add -> L.build_fadd
+          | A.Sub     -> L.build_fsub
+    	    | A.Mult    -> L.build_fmul
+    	    | A.Div     -> L.build_fdiv
+          | A.And     -> L.build_and
+    	    | A.Or      -> L.build_or
+    	    | A.Equal   -> L.build_icmp L.Icmp.Eq
+    	    | A.Neq     -> L.build_icmp L.Icmp.Ne
+    	    | A.Less    -> L.build_icmp L.Icmp.Slt
+    	    | A.Leq     -> L.build_icmp L.Icmp.Sle
+    	    | A.Greater -> L.build_icmp L.Icmp.Sgt
+    	    | A.Geq     -> L.build_icmp L.Icmp.Sge
+        ) e1' e2' "tmp" builder
+      | A.Unop(uop, e) ->
+        let e' = expr builder e in
+        (match uop with
+          A.Neg -> L.build_neg
+          | A.Not -> L.build_not
+        ) e' "tmp" builder
+      | _ -> raise (Failure "no pattern match") 
+        
     in 
-
-    let add_terminal builder instr =
-      match L.block_terminator (L.insertion_block func_builder) with
-	      Some _ -> ()
-        | None -> ignore (instr func_builder) in
 
     let rec stmt builder = function
       A.Expr ex -> ignore(expr func_builder ex); func_builder 
-      | A.Block sl -> List.fold_left stmt func_builder sl 
+      | A.Block sl -> List.fold_left stmt func_builder sl
+      | _ -> raise (Failure "stmt no pattern match")
     in
 
     let func_builder = stmt func_builder (Block fdecl.A.body) in
@@ -123,7 +155,7 @@ let translate (begin_block, loop_block, end_block, config_block) input_file =
       | A.BoolLit b -> L.const_int i1_t (if b then 1 else 0)
       | A.Call ("print", [e]) ->
         L.build_call printf_func [| string_format_str builder; (expr builder e) |] "printf" builder
-
+      | _ -> raise (Failure "no pattern match") 
     in 
 
     let rec stmt builder = function
@@ -154,6 +186,8 @@ let translate (begin_block, loop_block, end_block, config_block) input_file =
                         A.Void -> ""
                       | _ -> f ^ "_result") in
          L.build_call fdef (Array.of_list llargs) result builder
+      | _ -> raise (Failure "no pattern match") 
+          
     in 
 
     let rec stmt builder = function
@@ -168,12 +202,12 @@ let translate (begin_block, loop_block, end_block, config_block) input_file =
 
     (*--- Add terminal ---*)
     (* TODO: move this to build_function_body and just call it below for main return *)
-    let add_terminal builder instr =
+    (*let add_terminal builder instr =
       match L.block_terminator (L.insertion_block builder) with
 	      Some _ -> ()
-        | None -> ignore (instr builder) 
+        | None -> ignore (instr builder)
   
-    in
+    in*)
 
     (* Call the things that happen in main *)
     List.iter build_function_body (snd begin_block);
