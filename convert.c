f4cdf4796455636e3cdeb8d2762e4bc044d715e9
Melanie Sawyer <melaniensawyer@gmail.com>
2018-11-20 14:12:16 -0500

    remove quotation marks from string

--- convert.c
+++ convert.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /* This function converts a string to an int */
 int string_to_int(char *a)
 {
+  a++;
+  a[strlen(a)-1] = 0;
   return atoi(a);
 }
 
