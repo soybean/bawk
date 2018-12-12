a1d5f0a99855f95a7250fca03902f5655d141ca6
Ashley An <ashley.an@columbia.edu>
2018-12-11 21:53:40 -0500

    removed preventing dollar in all functions from semant for now

--- semant.ml
+++ semant.ml
@@ -104,7 +104,10 @@ let check (begin_list, loop_list, end_list, config_list) =
       | RgxLiteral l -> (Rgx, SRgxLiteral l)
       | Noexpr     -> (Void, SNoexpr)
       | Id s       -> (type_of_identifier s, SId s)
-      | Access a ->raise (Failure("access " ^ string_of_expr a ^ " should not be called in begin"))
+			| Access(a) as acc ->
+						let (a, a') = expr a in
+						if a <> Int then raise (Failure ("incorrect access in " ^ string_of_expr acc))
+						else (String, SAccess(a, a'))
       | ArrayLit(l) -> if List.length l > 0 then 
               let typ = expr(List.nth l 0) in
               let (arraytype, _) = typ in
