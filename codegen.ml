de2ad2cb4380972dee0cd728b35fb7abacf2ad37
Alfred Aho <victoria.j.yang@gmail.com>
2018-12-11 21:07:20 -0500

    fixed syntax

--- codegen.ml
+++ codegen.ml
@@ -265,7 +265,7 @@ let translate (begin_block, loop_block, end_block, config_block) =
         let arr_type = match ty with
         ArrayType t -> t
        | _ -> raise(Failure "not an array") in
-        let v = L.build_call arrayderef_func [| loopend_expr builder is_loop ar; loopend_expr builder is_loop idx |] "getElement" builder in
+        let v = L.build_call arrayderef_func [| expr builder ar; expr builder idx |] "getElement" builder in
         (match arr_type with
          A.String -> L.build_inttoptr v str_t "arrayDeref" builder
        | A.Int -> L.build_trunc v i32_t "arrayDeref" builder
