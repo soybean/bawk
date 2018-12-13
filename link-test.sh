650b325678c96123caef7b6f1acd828a9403c230
Ashley An <ashley.an@columbia.edu>
2018-12-13 18:27:04 -0500

    removed unnecessary script

--- link-test.sh
+++
@@ -1,7 +0,0 @@
-set -e
-./bawk.native -c tests/pass-config.bawk > int_to.ll
-llc -relocation-model=pic int_to.ll > int_to.s
-cc -o int_to.exe int_to.s convert.o structure.o mylist.o rgx.o
-./int_to.exe input.txt
-
-
